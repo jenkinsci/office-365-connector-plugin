3ba1b6937c8cd7f41e75cfc25b036bec525b2a12
Damian Szczepanik <damian.publicemail@gmail.com>
2017-10-18 23:00:39 +0200

    Tests for Macro class

--- src/main/java/jenkins/plugins/office365connector/model/Macro.java
+++ src/main/java/jenkins/plugins/office365connector/model/Macro.java
@@ -2,9 +2,9 @@
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -40,8 +40,8 @@ public class Macro extends AbstractDescribableImpl<Macro> {
 
     @DataBoundConstructor
     public Macro(String template, String value) {
-        this.template = Util.fixEmptyAndTrim(template);
-        this.value = Util.fixEmptyAndTrim(value);
+        this.template = Util.fixNull(template);
+        this.value = Util.fixNull(value);
     }
 
     public String getTemplate() {
@@ -60,6 +60,5 @@ public class Macro extends AbstractDescribableImpl<Macro> {
         public String getDisplayName() {
             return "Macro";
         }
-
     }
 }
---
+++ src/test/java/jenkins/plugins/office365connector/model/MacroTest.java
@@ -0,0 +1,55 @@
+package jenkins.plugins.office365connector.model;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import hudson.model.Descriptor;
+import org.junit.Test;
+
+/**
+ * @author Damian Szczepanik (damianszczepanik@github)
+ */
+public final class MacroTest {
+
+    @Test
+    public void newMacro_StoresPassedValues() {
+
+        // given
+        String template = "Michael";
+        String value = "Jackson";
+
+        // when
+        Macro macro = new Macro(template, value);
+
+        // then
+        assertThat(macro.getTemplate()).isEqualTo(template);
+        assertThat(macro.getValue()).isEqualTo(value);
+    }
+
+    @Test
+    public void newMacro_OnNullValue_StoresEmptyString() {
+
+        // given
+        String template = null;
+        String value = null;
+
+        // when
+        Macro macro = new Macro(template, value);
+
+        // then
+        assertThat(macro.getTemplate()).isEmpty();
+        assertThat(macro.getValue()).isEmpty();
+    }
+
+    @Test
+    public void getDisplayName_ReturnsName() {
+
+        // given
+        Descriptor<Macro> descriptor = new Macro.DescriptorImpl();
+
+        // when
+        String name = descriptor.getDisplayName();
+
+        // then
+        assertThat(name).isEqualTo("Macro");
+    }
+}
