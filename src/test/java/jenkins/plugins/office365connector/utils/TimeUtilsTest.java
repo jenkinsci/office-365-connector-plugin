123166a6c746022090c3a0f043a392deccf1006b
Damian Szczepanik <damian.publicemail@gmail.com>
2019-03-27 22:48:35 +0100

    Remove calculation for duration and use joda library

--- src/test/java/jenkins/plugins/office365connector/utils/TimeUtilsTest.java
+++ src/test/java/jenkins/plugins/office365connector/utils/TimeUtilsTest.java
@@ -58,25 +58,37 @@ public final class TimeUtilsTest {
         long duration = /* sec*/ 1 + /*min*/ 60 +/*hour*/ +60 * 60 +/*day*/+60 * 60 * 24;
 
         // when
-        String format = TimeUtils.durationToString(duration);
+        String format = TimeUtils.formatDuration(duration);
 
         // then
         assertThat(format).isEqualTo("1 day, 1 hour, 1 minute, 1 second");
     }
 
     @Test
+    public void durationToString_OnZeroSeconds_ReturnsDurationWithoutSeconds() {
+
+        // given
+        long duration = /* sec*/ 0 + /*min*/ 0 +/*hour*/ +60 * 60 +/*day*/+60 * 60 * 24;
+
+        // when
+        String format = TimeUtils.formatDuration(duration);
+
+        // then
+        assertThat(format).isEqualTo("1 day, 1 hour");
+    }
+
+    @Test
     public void durationToString_OnPluralValues_ReturnsDurationAsString() {
 
         // given
-        long duration = /* sec*/ 5 + /*min*/60 * 6 +/*hour*/ +60 * 60 * 7 +/*day*/+60 * 60 * 24 * 8;
+        long duration = /* sec*/ 5 + /*min*/60 * 6 +/*hour*/ +60 * 60 * 7 +/*day*/+60 * 60 * 24 * 5 +/*weeks*/+60 * 60 * 24 * 14;
 
         // when
-        String format = TimeUtils.durationToString(duration);
+        String format = TimeUtils.formatDuration(duration);
 
         // then
-        assertThat(format).isEqualTo("8 days, 7 hours, 6 minutes, 5 seconds");
+        assertThat(format).isEqualTo("2 weeks, 5 days, 7 hours, 6 minutes, 5 seconds");
     }
-
 
     @Test
     public void durationToString_OnOneSecond_ReturnsDurationOnlyWithSecond() {
@@ -85,7 +97,7 @@ public final class TimeUtilsTest {
         long duration = /* sec*/ 15;
 
         // when
-        String format = TimeUtils.durationToString(duration);
+        String format = TimeUtils.formatDuration(duration);
 
         // then
         assertThat(format).isEqualTo("15 seconds");
