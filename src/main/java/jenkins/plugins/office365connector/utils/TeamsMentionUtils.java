c4ba2899858e20c1eb11fbfe971668e5800e3648
Ammar Zain <Ammar.zain@geospectrum.ca>
2025-12-15 13:08:54 -0400

    added a toggle that will fetch user emails and wrap them in <at> tags so the users with extracted emails are tagged

---
+++ src/main/java/jenkins/plugins/office365connector/utils/TeamsMentionUtils.java
@@ -0,0 +1,33 @@
+package jenkins.plugins.office365connector.utils;
+
+import org.apache.commons.lang.StringUtils;
+import hudson.model.User;
+import hudson.tasks.Mailer;
+
+public final class TeamsMentionUtils {
+
+    private TeamsMentionUtils() {
+        // utility class
+    }
+
+    public static String mentionEmail(String email) {
+        if (StringUtils.isBlank(email)) {
+            return email;
+        }
+        return "<at>" + email + "</at>";
+    }
+
+    public static String mentionUser(User user) {
+    if (user == null) {
+        return "Unknown User";
+    }
+    Mailer.UserProperty prop = user.getProperty(Mailer.UserProperty.class);
+    if (prop != null && StringUtils.isNotBlank(prop.getAddress())) {
+        return mentionEmail(prop.getAddress());
+    }
+    // fallback: just return username
+    return user.getFullName();
+}
+
+
+}
